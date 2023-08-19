3d2127856030c601f818ce432735cf4ed3d4b869
bikkibro <bibektimilsina7857@gmail.com>
2023-08-19 12:59:28 +0545

    patch

--- routes/campaign.js
+++ routes/campaign.js
@@ -7,7 +7,7 @@ const upload=require('../props/fileupload')
 const uploadCredential=require('../props/credentialsUpload')
 
 router.route('/campaign').get(getAllCampaign).post(authorization,upload.single('img'),createCampaign)
-router.route('/campaign/:id').get(getCampaign).post(authorization,restrict('admin'),updateCampaign).delete(authorization,restrict('admin'),deleteCampaign)
+router.route('/campaign/:id').get(getCampaign).patch(authorization,restrict('admin'),updateCampaign).delete(authorization,restrict('admin'),deleteCampaign)
 router.route('/campaign/:id/credentials').get(getCredential).post(authorization,uploadCredential.array('document', 5),credential)
 router.route('/pendingcampaign').get(getPendingCampaign)
 module.exports=router;
